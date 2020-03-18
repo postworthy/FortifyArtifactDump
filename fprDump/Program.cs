7da5c77129ef27f8b731c82fe60c8a0fb5880762
Landon Key <>
2020-03-18 15:26:59 -0500

    Add project files.

---
+++ fprDump/Program.cs
@@ -0,0 +1,16 @@
+﻿using fprDump.fpr;
+using System;
+
+namespace fprDump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var dumper = new FprDumper(args[0]))
+            {
+                dumper.ExportSolution(args[1]);
+            }
+        }
+    }
+}
---
+++ fprDump/fpr/FprDumper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace fprDump.fpr
+{
+    public class FprDumper : IDisposable
+    {
+        private ZipArchive Archive { get; set; }
+        public FprDumper(string fprPath)
+        {
+            Archive = new ZipArchive(File.OpenRead(fprPath), ZipArchiveMode.Read, false);
+        }
+
+        public void ExportSolution(string outputDirectory)
+        {
+            XDocument index;
+            using (var str = Archive.GetEntry("src-archive/index.xml").Open())
+            {
+                index = XDocument.Load(str);
+            }
+            foreach(var entry in index.Descendants("entry"))
+            {
+                using (var str = Archive.GetEntry(entry.Value).Open())
+                using (var reader = new StreamReader(str))
+                {
+                    var content = reader.ReadToEnd();
+                    var name = entry.Attribute(XName.Get("key")).Value.Split("/").Last();
+                    File.WriteAllText(Path.Combine(outputDirectory, name), content);
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            Archive.Dispose();
+        }
+    }
+}
